aa3fe323f0cc5d69ccaf192aa4aa0ca3f7b40d54
Axel Javier Ayala Siles <axel.ayala@fundacion-jala.org>
2024-09-20 00:15:19 -0400

    feat: database config inyected to program

---
+++ src/Infraestructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+
+
+using ExpenseTrackerGroup3.Data;
+
+namespace ExpenseTrackerGroup3.Infraestructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.
+            AddDatabase(configuration);
+        return services;
+    }
+
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<DatabaseOptions>(configuration.GetSection(DatabaseOptions.ConnectionStrings));
+        services.AddScoped<IDbConnectionFactory, DbConnection>();
+        return services;
+    }
+}
