bdedd0ae596ad2ad3129ecbe58043bc3aac5e296
Axel Javier Ayala Siles <axel.ayala@fundacion-jala.org>
2024-09-22 15:38:46 -0400

    feat: interface budget service defined

---
+++ src/Services/Interfaces/IBudgetService.cs
@@ -0,0 +1,14 @@
+using Domain.DTOs;
+using Domain.Entities;
+
+namespace ExpenseTrackerGroup3.Services.Interfaces;
+
+public interface IBudgetService
+{
+    Task<Budget> AddBudgetAsync(Guid userId, CreateBudget budget);
+    Task<Budget> GetBudgetUserByMonthAsync(Guid userId, DateTime month);
+    Task<bool> UpdateBudgetAsync(Guid budgetId, CreateBudget budget);
+    Task<bool> DeleteBudgetAsync(Guid budgetId);
+    Task<decimal> GetRemainingBudgetAsync(Guid userId);
+    Task<bool> CheckBudgetStatusAsync(Guid userId, DateTime month);
+}
