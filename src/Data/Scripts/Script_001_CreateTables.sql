2b68ef6a40a83d2a3671c3383682bf3e7dd995cc
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2024-09-21 23:56:54 -0400

    fix: fix script name upper case for certain values

--- src/Data/Scripts/Script_001_CreateTables.sql
+++ src/Data/Scripts/Script_001_CreateTables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE
-    IF NOT EXISTS "User" (
+    IF NOT EXISTS "user" (
         Id UUID PRIMARY KEY,
-        "Name" VARCHAR(50) NOT NULL,
+        "name" VARCHAR(50) NOT NULL,
         Email VARCHAR(50) NOT NULL,
         PasswordHash VARCHAR(60) NOT NULL,
         CreatedAt DATE NOT NULL
@@ -10,11 +10,11 @@ CREATE TABLE
 CREATE TABLE
     IF NOT EXISTS Expense (
         Id UUID PRIMARY KEY,
-        UserId UUID REFERENCES "User" (Id) ON DELETE CASCADE,
+        UserId UUID REFERENCES "user" (Id) ON DELETE CASCADE,
         Amount DECIMAL(10, 2) NOT NULL,
-        "Description" VARCHAR(255) NOT NULL,
+        "description" VARCHAR(255) NOT NULL,
         Category VARCHAR(50) NOT NULL,
-        "Date" DATE NOT NULL,
+        "date" DATE NOT NULL,
         CreatedAt DATE NOT NULL,
         RecurringExpense BOOLEAN NOT NULL
     );
@@ -22,7 +22,7 @@ CREATE TABLE
 CREATE TABLE
     IF NOT EXISTS Income (
         Id UUID PRIMARY KEY,
-        UserId UUID REFERENCES "User" (Id) ON DELETE CASCADE,
+        UserId UUID REFERENCES "user" (Id) ON DELETE CASCADE,
         Amount DECIMAL(10, 2) NOT NULL,
         Source VARCHAR(50) NOT NULL,
         CreatedAt DATE NOT NULL
@@ -31,8 +31,8 @@ CREATE TABLE
 CREATE TABLE
     IF NOT EXISTS Budget (
         Id UUID PRIMARY KEY,
-        UserId UUID REFERENCES "User" (Id) ON DELETE CASCADE,
-        "Month" DATE NOT NULL,
+        UserId UUID REFERENCES "user" (Id) ON DELETE CASCADE,
+        "month" DATE NOT NULL,
         BudgetAmount DECIMAL(10, 2) NOT NULL,
         AlertThreshold DECIMAL(10, 2) NULL
     );
@@ -40,7 +40,7 @@ CREATE TABLE
 CREATE TABLE
     IF NOT EXISTS Goal (
         Id UUID PRIMARY KEY,
-        UserId UUID REFERENCES "User" (Id) ON DELETE CASCADE,
+        UserId UUID REFERENCES "user" (Id) ON DELETE CASCADE,
         GoalAmount DECIMAL(10, 2) NOT NULL,
         DeadLine DATE NOT NULL,
         CurrentAmount DECIMAL(10, 2) NOT NULL,
