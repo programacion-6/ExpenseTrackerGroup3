5c30efef3d51060954874316bf1e9c0cc2876579
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2024-09-24 00:15:28 -0400

    refactor: refactor expense entity

--- src/Domain/Entities/Expense.cs
+++ src/Domain/Entities/Expense.cs
@@ -5,8 +5,8 @@ public class Expense
   public Guid UserId { get; set; }
   public decimal Amount { get; set; }
   public string? Description { get; set; }
-  public string? Category { get; set; }
+  public required string Category { get; set; }
   public DateTime Date { get; set; }
   public DateTime CreatedAt { get; set; }
-  public bool RecurringExpese { get; set; }
+  public bool RecurringExpense { get; set; }
 }
