54b1942584945564048ff5ec0ef41da935472eed
unknown <ronaldo.mendoza.60098@usalesiana.edu.bo>
2024-09-24 20:04:15 -0400

    refactor: delete MonthlyIncomeByUser and update month, userId

--- src/Repositories/Interfaces/IIncomeRepository.cs
+++ src/Repositories/Interfaces/IIncomeRepository.cs
@@ -3,6 +3,4 @@ using Domain.Entities;
 namespace ExpenseTrackerGroup3.Repositories.Interfaces;
 
 public interface IIncomeRepository : IRepository<Income>
-{
-    Task<Income?> GetMonthlyIncomeByUserId(Guid userId, DateTime month);
-}
+{}
