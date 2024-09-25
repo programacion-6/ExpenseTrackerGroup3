2fccc0bff7d9e9f68f124c1aecc2bf7e66109da0
Axel Javier Ayala Siles <axel.ayala@fundacion-jala.org>
2024-09-25 10:05:26 -0400

    feat: user service implemented

--- src/Services/Interfaces/IUserService.cs
+++ src/Services/Interfaces/IUserService.cs
@@ -6,6 +6,6 @@ namespace ExpenseTrackerGroup3.Services.Interfaces;
 
 public interface IUserService
 {
-    Task<User> GetUserProfileAsync(Guid userId);
+    Task<User?> GetUserProfileAsync(Guid userId);
     Task<User> UpdateUserProfileAsync(Guid userId, UpdateUserDTO user);
 }
--- src/Services/UserService.cs
+++ src/Services/UserService.cs
@@ -2,19 +2,49 @@
 using Domain.Entities;
 
 using ExpenseTrackerGroup3.Domain.DTOs;
+using ExpenseTrackerGroup3.Repositories.Interfaces;
 using ExpenseTrackerGroup3.Services.Interfaces;
 
 namespace ExpenseTrackerGroup3.Services;
 
 public class UserService : IUserService
 {
-    public Task<User> GetUserProfileAsync(Guid userId)
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
     {
-        throw new NotImplementedException();
+        _userRepository = userRepository;
+    }
+    
+    public async Task<User?> GetUserProfileAsync(Guid userId)
+    {
+        var userExists = _userRepository.GetByIdAsync(userId);
+
+        if (userExists == null)
+        {
+            throw new ArgumentException("User not found");
+        }
+
+        return await userExists;
     }
 
-    public Task<User> UpdateUserProfileAsync(Guid userId, UpdateUserDTO user)
+    public async Task<User> UpdateUserProfileAsync(Guid userId, UpdateUserDTO user)
     {
-        throw new NotImplementedException();
+        var userExists = await _userRepository.GetByIdAsync(userId);
+
+        if (userExists == null)
+        {
+            throw new ArgumentException("User not found");
+        }
+
+        var updatedUser = user.ToDomain(userExists);
+        var success = await  _userRepository.UpdateAsync(updatedUser);
+        
+        if (!success)
+        {
+            throw new Exception("Failed to update user");
+        }
+
+        return updatedUser;
     }
 }
