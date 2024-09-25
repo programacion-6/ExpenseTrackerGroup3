b802e90def215442699c3554b21de296b44d65d7
unknown <ronaldo.mendoza.60098@usalesiana.edu.bo>
2024-09-24 20:08:01 -0400

    refactor: IncomeControllers userId

--- src/Controllers/IncomeController.cs
+++ src/Controllers/IncomeController.cs
@@ -1,4 +1,6 @@
 using Domain.DTOs;
+using Domain.Entities;
+
 using ExpenseTrackerGroup3.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,8 +23,25 @@ public class IncomeController : BaseController
     try
     {
       var newIncome = await _incomeService.AddIncomeAsync(userId, income);
-      var response = ResponseIncome.FromDomain(newIncome);
-      return CreatedAtAction(nameof(GetMonthlyIncome), new { userId, month = DateTime.Now }, response);
+
+      return Ok(newIncome);
+    }
+    catch (Exception e)
+    {
+      return HandleException(e);
+    }
+  }
+
+  [HttpGet("{userId}")]
+  [ProducesResponseType(typeof(ResponseIncome), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  public async Task<IActionResult> GetIncomesByUserId(Guid userId)
+  {
+    try
+    {
+      IEnumerable<Income> income = await _incomeService.GetIncomesByUserIdAsync(userId);
+
+      return Ok(income);
     }
     catch (Exception e)
     {
@@ -38,8 +57,8 @@ public class IncomeController : BaseController
     try
     {
       var income = await _incomeService.GetMonthlyIncomeByUserId(userId, month);
-      var response = ResponseIncome.FromDomain(income);
-      return Ok(response);
+
+      return Ok(income);
     }
     catch (Exception e)
     {
@@ -47,54 +66,17 @@ public class IncomeController : BaseController
     }
   }
 
-  [HttpPut("{userId}/{incomeId}")]
+  [HttpPut("{userId}")]
   [ProducesResponseType(typeof(ResponseIncome), StatusCodes.Status200OK)]
   [ProducesResponseType(StatusCodes.Status400BadRequest)]
   [ProducesResponseType(StatusCodes.Status404NotFound)]
-  public async Task<IActionResult> UpdateIncome(Guid userId, Guid incomeId, [FromBody] CreateIncome income)
+  public async Task<IActionResult> UpdateIncome(Guid userId, [FromBody] UpdateIncome income)
   {
     try
     {
-      var updatedIncome = await _incomeService.UpdateIncomeAsync(userId, incomeId, income);
-      var response = ResponseIncome.FromDomain(updatedIncome);
-      return Ok(response);
-    }
-    catch (Exception e)
-    {
-      return HandleException(e);
-    }
-  }
+      var updatedIncome = await _incomeService.UpdateIncomeAsync(userId, income);
 
-  [HttpDelete("{incomeId}/{userId}")]
-  [ProducesResponseType(StatusCodes.Status204NoContent)]
-  [ProducesResponseType(StatusCodes.Status404NotFound)]
-  public async Task<IActionResult> DeleteIncome(Guid incomeId, Guid userId)
-  {
-    try
-    {
-      var success = await _incomeService.DeleteIncomeAsync(incomeId, userId);
-      if (success)
-      {
-        return NoContent();
-      }
-      return NotFound();
-    }
-    catch (Exception e)
-    {
-      return HandleException(e);
-    }
-  }
-
-  [HttpGet("{userId}")]
-  [ProducesResponseType(typeof(ResponseIncome), StatusCodes.Status200OK)]
-  [ProducesResponseType(StatusCodes.Status404NotFound)]
-  public async Task<IActionResult> GetIncomesByUserId(Guid userId)
-  {
-    try
-    {
-      var income = await _incomeService.GetIncomesByUserIdAsync(userId);
-      var response = ResponseIncome.FromDomain(income);
-      return Ok(response);
+      return Ok(updatedIncome);
     }
     catch (Exception e)
     {
